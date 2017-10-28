80b12a285647c3479f5e258567fb2d112523e127
DarkSt0rm <DarkSt0rm@users.noreply.github.com>
2017-10-28 21:50:56 +0200

    Update build-masscan.sh

--- ubuntu/build-masscan.sh
+++ ubuntu/build-masscan.sh
@@ -3,7 +3,7 @@ apt-get install git gcc make libpcap-dev clang git -y
 git clone https://github.com/robertdavidgraham/masscan.git
 cd masscan
 make -j
-mv .\bin\masscan ..\..\masscan_bin
-cd ..\
+mv ./bin/masscan ../masscan_bin
+cd ../
 rm -rf masscan
 mv masscan_bin masscan
